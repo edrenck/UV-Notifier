8831a40180aba2df6085087b5ab04d285ad95224
Edwin Renck <edrenck@gmail.com>
2021-03-28 00:38:27 -0700

    Made google api response call openweather, and started on ui
    Co-authored-by: thatweebdev@gmail.com <thatweebdev@gmail.com>
    Co-authored-by: Eric Huang <HE-1234@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,39 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+//UI imports
+import { Provider as PaperProvider, TopBar, MainScreen} from "react-native-paper";
 
 export default function App() {
 
-  const [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState([]);
+  const [zipcode, setZipcode] = useState("");
+  const [googleResponse, setGoogleResponse] = useState([]);
+  const [runonce, setRunOnce] = useState(true);
+  const [weatherInfo, setWeatherInfo] = useState("")
 
-  let weatherInfo = null
 
-  let url = "https://pro.openweathermap.org/data/2.5/onecall";
-  let lat = "32.2226";
-  let lon = "-110.9747";
+  let weatherURL = "https://pro.openweathermap.org/data/2.5/onecall";
 
-  const dataFromAPI = async () => {
+  let googleURL = "https://maps.googleapis.com/maps/api/geocode/json?";
+
+  const googleAPI = async () => {
     try {
-      const response = await fetch(`${url}?lat=${lat}&lon=${lon}&appid=${key}`);
+      const response = await fetch(`${googleURL}address=${zipcode}&key=${googleKEY}`);
       const data = await response.json();
-      console.log(data);
-      setWeather(data);
+      setGoogleResponse(data.results[0]);
     } catch (error) {
       console.error(error);
     }
   }
 
-  if(weather) {
-    weatherInfo = JSON.stringify(weather);
+  const openWeatherAPI = async () => {
+    try {
+      const response = await fetch(`${weatherURL}?lat=${googleResponse.geometry.location.lat}&lon=${googleResponse.geometry.location.lng}&appid=${key}`);
+      const data = await response.json();
+      setWeather(data);
+      console.log(data);
+      let max = 0;
+      for (let i = 0; i < 24; i++) {
+        if (data.hourly[i].uvi > max) {
+          max = data.hourly[i].uvi;
+        }
+      }
+      setWeatherInfo(max);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  if (runonce && googleResponse.length != 0) {
+    openWeatherAPI();
+    setRunOnce(false);
   }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button onPress={dataFromAPI} title="Tester Button"/>
+      <TextInput 
+        style={{
+          height: 40,
+          borderColor: "blue"
+          
+        }}
+        placeholder="Type your zipcode here!"
+        onChangeText = {text => setZipcode(text)}
+      />
+      <Button onPress={googleAPI} title="Tester Button"/>
       <Text>Stuff will show up here</Text>
-      <Text>{weatherInfo}</Text>
+      <Text style={{color: getHex(weatherInfo)}}>{weatherInfo}</Text>
       <StatusBar style="auto" />
+
+
+
+
+        {/* UI thingy */}
+        {/* <PaperProvider>
+            <TopBar />
+
+
+
+
+             <MainScreen />
+        </PaperProvider> */}
     </View>
   );
 }
@@ -46,3 +90,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function getHex(num) {
+  if(num <= 2){
+    return "#13bf00"
+  } else if(num <= 5){
+    return "#f1f50a"
+  } else if(num <= 7){
+    return "#ff9d00"
+  } else if(num < 10){
+    return "#c91c16"
+  } else{
+    return "#9002a6"
+  }
+}
+
+
+const textStyle = StyleSheet.create({
+  
+})
+
